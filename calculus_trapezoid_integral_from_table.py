5dc955b04dee26f65465a34d21ef247f712100d4
mecaneer23 <mecaneer23@gmail.com>
2024-01-20 00:06:18 -0600

    feature: use get_trapezoid function in more pure format

--- calculus_trapezoid_integral_from_table.py
+++ calculus_trapezoid_integral_from_table.py
@@ -1,17 +1,38 @@
 #!/usr/bin/env python3
 # pylint: disable=missing-docstring
 
-from itertools import chain, pairwise, starmap
+from itertools import chain, islice, pairwise, starmap, tee
 from operator import sub
 
-x_values = pairwise(list(map(int, input("enter x values: ").split())))
-fx_values = pairwise(list(map(int, input("enter f(x) values: ").split())))
+x_values = pairwise(list(map(float, input("enter x values: ").split())))
+fx_values, fx_values_ = tee(
+    chain.from_iterable(
+        pairwise(list(map(float, input("enter f(x) values: ").split())))
+    ),
+    2,
+)
 
 
-def get_trapezoid(height: int, f1_f2: tuple[int, int]) -> float:
-    f1, f2 = f1_f2
+def get_trapezoid(height: float, f1: float, f2: float) -> float:
     return 1 / 2 * height * (f1 + f2)
 
 
 heights = list(starmap(sub, tuple(map(reversed, x_values))))
-print(sum(list(starmap(get_trapezoid, zip(heights, chain(fx_values))))))
+print(
+    sum(
+        list(
+            starmap(
+                get_trapezoid,
+                zip(
+                    heights,
+                    islice(fx_values, 0, None, 2),
+                    islice(fx_values_, 1, None, 2),
+                ),
+            )
+        )
+    )
+)
+
+# 0 2 5 7 8
+# 0 4 13 21 23
+# 85.5
